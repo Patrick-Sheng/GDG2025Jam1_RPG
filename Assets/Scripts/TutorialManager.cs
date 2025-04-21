c6be5a974bc2658779f9cc75a57fded2b20cc74f
Patrick <sheng.patrick@gmail.com>
2025-04-21 13:55:11 +1200

    feat: created a tutorial manager that shows instructions in-game

---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,52 @@
+using TMPro;
+using UnityEngine;
+using System.Collections;
+
+public class TutorialManager : MonoBehaviour
+{
+    
+    public TextMeshProUGUI instructionText;
+    private int progressIndex;
+
+    void Start()
+    {
+      progressIndex = 0;
+      updateInstruction();
+    }
+
+    // Update is called once per frame
+    void Update() 
+    {
+      switch(progressIndex) {
+        case 1:
+          if(Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow)) {
+            updateInstruction();
+          }
+          break;
+        case 2:
+          if(Input.GetKeyDown(KeyCode.C)) {
+            updateInstruction();
+          }
+          break;
+        default:
+          break;
+      } 
+    }
+
+    private void updateInstruction() {
+      progressIndex++;
+
+      switch(progressIndex) {
+        case 1:
+          instructionText.text = "Press arrow keys to move";
+          break;
+        case 2:
+          instructionText.text = "Press c to interact";
+          break;
+        default:
+          instructionText.text = "Hello!";
+          break;
+      }
+
+    }
+}
